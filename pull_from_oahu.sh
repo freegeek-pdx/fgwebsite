48557d074c1ebed903b838bd4e5a0374114aa9ae
Ryan Niebur <ryanryan52@gmail.com>
2011-09-17 12:55:14 -0700

    add a script to control the dumping, scping, and reloading of the wordpress database. the firewall always makes that difficult.

---
+++ pull_from_oahu.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+REMOTE=oahu
+LOCAL=press
+
+set -e
+
+HOSTNAME=$(hostname)
+
+if [ "$HOSTNAME" = "$REMOTE" -o "$HOSTNAME" = "$LOCAL" ]; then
+    echo "Run this on your own machine, it only works internally"
+    exit 1
+fi
+
+#ssh "$REMOTE" /usr/share/wordpress/wp-content/db.sh dump file.sql
+scp "${REMOTE}:file.sql" ./
+scp file.sql "${LOCAL}:"
+ssh "$LOCAL" /usr/share/wordpress/wp-content/db.sh load file.sql
