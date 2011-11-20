295d3035bbe5d680bfa2f7e1dd9a1b0c5b4bbd24
Ryan Niebur <ryanryan52@gmail.com>
2011-11-20 13:37:47 -0800

    complete the new code and disable it until I add syncing of the directory of uploaded files, too

--- db.sh
+++ db.sh
@@ -2,17 +2,20 @@
 
 set -e
 
+# TODO: rsync or otherwise combine the uploaded files and then fix the FIXMEs below
+# they are saved in /usr/share/wordpress/wp-content/uploads
+
 config() {
     TEMPF=$(tempfile)
     sudo cat /etc/wordpress/config-*.php | grep DB_ | grep define | cut -d "'" -f 2,4 | sed "s/'/=/" | sort -u > "$TEMPF"
     . "$TEMPF"
     # not used outside of config() anymore
     URL=$(echo "SELECT option_value FROM wp_options WHERE option_name = 'siteurl';" | mysql -u $DB_USER -p$DB_PASSWORD $DB_NAME | tail -1)
-#    NEWS_URL="${URL}/newsletter"
+    NEWS_URL="${URL}/newsletter"
     BASE_URL="$(basename "$URL")"
-    if [ "$BASE_URL" eq "www.freegeek.org" ]; then
+    if [ "$BASE_URL" = "www.freegeek.org" ]; then
 	OTHER_URL="testwww.freegeek.org"
-    elif [ "$BASE_URL" eq "testwww.freegeek.org" ]; then
+    elif [ "$BASE_URL" = "testwww.freegeek.org" ]; then
 	OTHER_URL="www.freegeek.org"
     else
 	echo "ERROR: Unknown BASE_URL: $BASE_URL"
@@ -31,16 +34,18 @@ usage() {
 }
 
 if [ -z "$1" -o -z "$2" ]; then
-    if [ "$1" ne "mysql" ]; then
+    if [ "$1" != "mysql" ]; then
 	usage
     fi
 fi    
 
 parse_sql_dump() {
+    cat "$1"   # FIXME
+    return     # FIXME
     if echo "$BASE_URL" | grep -q "$OTHER_URL"; then
-	sed -e "s/${BASE_URL}/${OTHER_URL}/g" -e "s/${OTHER_URL}/${BASE_URL}/g" "$2"
+	sed -e "s/${BASE_URL}/${OTHER_URL}/g" -e "s/${OTHER_URL}/${BASE_URL}/g" "$1"
     else
-	sed "s/${OTHER_URL}/${BASE_URL}/g" "$2"
+	sed "s/${OTHER_URL}/${BASE_URL}/g" "$1"
     fi
 }
 
@@ -55,9 +60,8 @@ do_mysql() {
 case "$1" in
     load)
 	config
-	parse_sql_dump | do_mysql
-	# handled with the sed, now
-	# echo "UPDATE wp_options SET option_value = '$URL' WHERE option_name = 'siteurl'; UPDATE news_wp_options SET option_value = '$NEWS_URL' WHERE option_name = 'siteurl';" | do_mysql
+	parse_sql_dump "$2" | do_mysql
+	echo "UPDATE wp_options SET option_value = '$URL' WHERE option_name = 'siteurl'; UPDATE news_wp_options SET option_value = '$NEWS_URL' WHERE option_name = 'siteurl';" | do_mysql # FIXME: handled with the sed, now
 	;;
     dump)
 	config
